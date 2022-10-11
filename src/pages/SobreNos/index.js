9d21ce2366e11a79b97ab9c0338f33650da6b929
ChrisFigue <111028872+ChrisFigue@users.noreply.github.com>
2022-10-11 00:15:57 -0300

    Add files via upload

---
+++ src/pages/SobreNos/index.js
@@ -0,0 +1,11 @@
+function Sobrenos() {
+    return ( 
+        <div>
+            <h1> SOBRE NÓS </h1>
+            <p>
+                Nós somos uma empresa de transporte de mercadorias, contrate nosso serviço, você não vai se arrepender!
+            </p>
+        </div>
+    )
+}
+export default Sobrenos;

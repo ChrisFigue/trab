cb7077316d084fb2acf2e9de37dee748fbe30722
ChrisFigue <111028872+ChrisFigue@users.noreply.github.com>
2022-10-12 16:15:23 -0300

    Add files via upload

---
+++ src transporte rodoviario/Routes.js
@@ -0,0 +1,24 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import CadastroCliente from "./pages/CadastroCliente";
+import CalculodoFrete from "./pages/CalculodoFrete";
+import Login from "./pages/Login";
+import SobreNos from "./pages/SobreNos";
+
+import Header from './Components/Header/index.js';
+
+
+
+function RoutesApp () {
+    return <BrowserRouter>
+    <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/Login" element={<Login/>} />
+            <Route path="/CadastroCliente" element={<CadastroCliente/>} />
+            <Route path="/CalculodoFrete" element={<CalculodoFrete/>} />
+            <Route path="/SobreNos" element={<SobreNos/>} />
+        </Routes>
+    </BrowserRouter>
+}
+export default RoutesApp;
---
+++ src transporte rodoviario/pages/Home/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return ( 
+        <div align = "center">
+            <h1> PÁGINA HOME</h1>
+            <p>
+            Tela Inicial da nossa empresa de serviço de transporte!
+            </p>
+        </div>
+    )
+}
+export default Home;
